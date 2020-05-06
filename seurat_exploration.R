ac6a0137e6235dbeb36d574b1da116512af9b687
rpolicastro <rpolicas@indiana.edu>
2020-05-05 22:00:50 -0400

    finished preliminary background correction for tdT

--- seurat_exploration.R
+++ seurat_exploration.R
@@ -29,9 +29,11 @@ if (!dir.exists(file.path("results", "tdTomato"))) {
 
 ## Plotting the expression of tdTomato and tdTomato 5' UTR.
 
+DefaultAssay(seurat_integrated) <- "RNA"
+
 p <- FeaturePlot(
 	seurat_integrated, feature = c("tdTomato", "tdTomatoStop", "Pax7"),
-	pt.size = 0.1, ncol = 2, split.by = "ident"
+	pt.size = 0.1, ncol = 2, split.by = "orig.ident"
 )
 
 pdf(file.path("results", "tdTomato", "tdTomato_dimplot.pdf"), height = 12, width = 16)
@@ -53,7 +55,7 @@ raw_counts <- raw_counts %>%
 	as.data.table %>%
 	transpose(keep.names = "cell_id", make.names = 1)
 
-raw_counts[, tdT_ratio := (tdTomato + 1) / (tdTomatoStop + 1)]
+raw_counts[, tdT_Ratio := (tdTomato + 1) / (tdTomatoStop + 1)]
 
 ## Grabbing cell meta data.
 
@@ -65,27 +67,45 @@ meta_data <- as.data.table(seurat_integrated@meta.data, keep.rownames = "cell_id
 
 merged <- merge(meta_data, raw_counts, by = "cell_id")
 
-## Preparing the negative control and test set.
+## Preparing the negative control set.
 
-neg_control <- merged[orig.ident == "tdT_Parental"][, .(orig.ident, cell_id, tdT_ratio)]
-experimental <- merged[orig.ident == "KY_mononuclear"][, .(orig.ident, cell_id, tdT_ratio)]
+neg_control <- merged[orig.ident == "tdT_Parental"][, .(orig.ident, cell_id, tdT_Ratio)]
 
-## Permutation test.
+## prediction interval test.
 
-permute_rank <- function(combined_data) {
-	permuted_rank <- sample_frac(combined_data, 1L)[1, "rank"]
-	return(permuted_rank)
-}
-
-permutation_results <- map_df(experimental[["cell_id"]], function(x) {
-
-	combined <- rbind(neg_control, row_args)
-	combined[, rank := dense_rank(-tdT_ratio)]
-
-	actual_rank <- combined[cell_id == row_args[1, "cell_id"]][["rank"]]
-	perm_result <- replicate(10, permute_rank(combined))
-	perm_result <- sum(perm_result <= actual_rank)
-
-	row_args[["pval"]] <- (perm_result + 1) / (10 + 1)
-	return(row_args)
+exact_results <- map(unique(merged[["orig.ident"]]), function(x) {
+	pred_interval <- pmap_df(merged[orig.ident == x], function(...) {
+		row_args <- data.table(...)
+		pval <- sum(neg_control[["tdT_Ratio"]] >= row_args[["tdT_Ratio"]]) / nrow(neg_control)
+		row_args[["pval"]] <- pval
+		return(row_args)
+	})
+	pred_interval[, tdT_Ratio_FDR := p.adjust(pval, "fdr")]
+	pred_interval[, tdT_High := ifelse(tdT_Ratio_FDR < 0.05, TRUE, FALSE)]
+	return(pred_interval)
 })
+
+exact_results <- rbindlist(exact_results)
+
+## Add results back to seurat object.
+
+plot_results <- exact_results[,
+	.(cell_id, tdT_Ratio, tdT_Ratio_FDR, tdT_High)
+][
+	order(match(cell_id, rownames(seurat_integrated@meta.data)))
+]
+
+seurat_integrated[["tdT_Ratio"]] <- plot_results[["tdT_Ratio"]]
+seurat_integrated[["tdT_Ratio_FDR"]] <- plot_results[["tdT_Ratio_FDR"]]
+seurat_integrated[["tdT_High"]] <- plot_results[["tdT_High"]]
+
+## Plot Results.
+
+p <- FeaturePlot(
+	seurat_integrated, split.by = "orig.ident", pt.size = 0.01,
+	features = c("tdT_Ratio", "tdT_Ratio_FDR", "tdT_High")
+)
+
+pdf(file.path("results", "tdTomato", "tdT_Ratio.pdf"), height = 12, width = 16)
+p
+dev.off()
