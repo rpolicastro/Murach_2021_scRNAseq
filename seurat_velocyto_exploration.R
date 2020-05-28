1e4e30b5a8957411497e6124fd7072e99933a475
rpolicastro <rpolicas@indiana.edu>
2020-05-28 15:54:13 -0400

    started making RNA velocity plots

--- seurat_velocyto_exploration.R
+++ seurat_velocyto_exploration.R
@@ -12,6 +12,8 @@ library("future")
 library("wesanderson")
 library("readxl")
 library("scProportionTest")
+library("velocyto.R")
+library("SeuratWrappers")
 
 ###################################
 ## Exploration of scRNA-seq Data ##
@@ -20,7 +22,7 @@ library("scProportionTest")
 ## Load Integrated Data
 ## ----------
 
-seurat_integrated <- readRDS(file.path("results", "r_objects", "seurat_integrated.RDS"))
+seurat_integrated <- readRDS(file.path("results", "r_objects", "seurat_integrated_spliced.RDS"))
 
 ##################
 ## Marker Genes ##
@@ -58,3 +60,42 @@ fwrite(
 	markers, file.path("results", "markers", "marker_table_spliced.tsv"),
 	col.names = TRUE, row.names = FALSE, quote = FALSE, sep = "\t"
 )
+
+##################
+## RNA Velocity ##
+##################
+
+if (!dir.exists(file.path("results", "rna_velocity"))) {
+        dir.create(file.path("results", "rna_velocity"), recursive = TRUE)
+}
+
+## Run RNA velocity.
+
+Idents(seurat_integrated) <- "integrated_snn_res.0.5"
+
+walk(unique(seurat_integrated[["orig.ident"]][[1]]), function(sc_sample) {
+
+	test_subset <- subset(seurat_integrated, subset = orig.ident == sc_sample, downsample = 250)
+
+	seurat_velocity <- RunVelocity(
+		test_subset, ambiguous = "ambiguous", ncores = 6,
+		deltaT = 1, kCells = 25, fit.quantile = 0.02,
+		group.by = "integrated_snn_res.0.5"
+	)
+
+	ident.colors <- (scales::hue_pal())(n = length(x = levels(x = seurat_velocity)))
+	names(x = ident.colors) <- levels(x = seurat_velocity)
+	cell.colors <- ident.colors[Idents(object = seurat_velocity)]
+	names(x = cell.colors) <- colnames(x = seurat_velocity)
+
+	pdf(file.path("results", "rna_velocity", str_c(sc_sample, "_velocity.pdf")), height = 8, width = 8)
+	show.velocity.on.embedding.cor(
+		emb = Embeddings(object = seurat_velocity, reduction = "umap"),
+		vel = Tool(object = seurat_velocity, slot = "RunVelocity"),
+		n = 200, scale = "sqrt", cell.colors = ac(x = cell.colors, alpha = 0.5), 
+		cex = 0.8, arrow.scale = 3, show.grid.flow = TRUE,
+		min.grid.cell.mass = 0.5, grid.n = 40, arrow.lwd = 1, do.par = FALSE,
+		cell.border.alpha = 0.1, n.cores = 6
+	)
+	dev.off()
+})
